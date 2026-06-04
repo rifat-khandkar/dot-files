2b7480745397fd0ae961ca987c7c241fc5e99701
rifat-khandkar <rifatkhandkar@proton.me>
2026-06-04 15:49:56 +0530

    Update bluetooth.sh
    the bluetooth icon was not changing colour to muted when bt is powered off. the issue is fixed now.

--- config/waybar/scripts/bluetooth.sh
+++ config/waybar/scripts/bluetooth.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-if ! rfkill list bluetooth 2>/dev/null | grep -q "Bluetooth"; then
-  printf '{"text": "", "class": "hidden"}\n'
-  exit 0
-fi
-if rfkill list bluetooth 2>/dev/null | grep -q "Soft blocked: yes"; then
-  printf '{"text": "", "class": "off"}\n'
-  exit 0
-fi
-if ! bluetoothctl show 2>/dev/null | grep -q "Powered: yes"; then
-  printf '{"text": "", "class": "off"}\n'
-  exit 0
-fi
-if bluetoothctl devices Connected 2>/dev/null | grep -q "^Device"; then
-  printf '{"text": "", "class": "connected"}\n'
-else
-  printf '{"text": "", "class": "on"}\n'
-fi
+while true; do
+  if ! rfkill list bluetooth 2>/dev/null | grep -q "Bluetooth"; then
+    printf '{"text": "", "class": "hidden"}\n'
+  elif rfkill list bluetooth 2>/dev/null | grep -q "Soft blocked: yes"; then
+    printf '{"text": "", "class": "off"}\n'
+  elif ! bluetoothctl show 2>/dev/null | grep -q "Powered: yes"; then
+    printf '{"text": "", "class": "off"}\n'
+  elif bluetoothctl devices Connected 2>/dev/null | grep -q "^Device"; then
+    printf '{"text": "", "class": "connected"}\n'
+  else
+    printf '{"text": "", "class": "on"}\n'
+  fi
+  sleep 0.5
+done
